8250c0c0d9a573d32af26934d1863b036db24bea
sebasL0615 <sebastian-lopez0226@outlook.com>
2026-04-29 16:06:56 -0500

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SESSION="mc"
+WEBHOOK="your-discordapp"
+
+curl -H "Content-Type: application/json" \
+-d '{"content":"Minecraft server restarting in 5 minutes!"}' \
+"$WEBHOOK"
+
+screen -S "$SESSION" -X stuff "Server restarting in 5 minutes!\n"
+sleep 240
+
+curl -H "Content-Type: application/json" \
+-d '{"content":"Minecraft server restarting in 1 minute!"}' \
+"$WEBHOOK"
+
+screen -S "$SESSION" -X stuff "Server restarting in 1 minute!\n"
+sleep 50
+
+screen -S "$SESSION" -X stuff "Server restarting in 10 seconds!\n"
+sleep 10
+
+curl -H "Content-Type: application/json" \
+-d '{"content":"Minecraft server shutting down for maintenance."}' \
+"$WEBHOOK"
+
+screen -S "$SESSION" -X stuff "Restarting now!\n"
+sleep 2
+screen -S "$SESSION" -X stuff "stop\n"
